448e154bcf22683701584c814df090c473e73379
a.vinsonneau <antoine.vinsonneau@compufirst.com>
2023-03-02 16:21:11 +0100

    Init Bootstrap

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
 
+def index(request):
+    # voiture_list = Voiture.objects.all()
+    # context = {'voiture_list': voiture_list}
+    return render(request, 'index.html')
